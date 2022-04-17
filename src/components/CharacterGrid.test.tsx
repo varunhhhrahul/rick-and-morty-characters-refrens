e3efa78d1331f70f6f6fad2d24cb04a2249943c0
Varun Ramnani <varunhhhrahul@gmail.com>
2022-04-18 01:14:48 +0530

    updates

--- src/components/CharacterGrid.test.tsx
+++ src/components/CharacterGrid.test.tsx
@@ -1,17 +1,28 @@
+import { act } from "@testing-library/react";
 import React from "react";
 import { Provider } from "react-redux";
 import renderer from "react-test-renderer";
 import store from "../app/store";
 import { CharacterGrid } from "./CharacterGrid";
 
+let component: renderer.ReactTestRenderer | null = null;
+
 describe("CharacterGrid", () => {
+  beforeEach(async () => {
+    await act(async () => {
+      component = renderer.create(
+        <Provider store={store}>
+          <CharacterGrid />
+        </Provider>
+      );
+    });
+  });
+
+  afterEach(() => {
+    component = null;
+  });
   test("should render correctly", async () => {
-    const component = renderer.create(
-      <Provider store={store}>
-        <CharacterGrid />
-      </Provider>
-    );
-    const tree = component.toJSON();
+    const tree = component!.toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
