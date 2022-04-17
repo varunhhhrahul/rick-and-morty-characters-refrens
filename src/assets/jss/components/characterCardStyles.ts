a0710bf041cedcd7ff6d88f95df82fb2318feb71
Varun Ramnani <varunhhhrahul@gmail.com>
2022-04-17 21:05:28 +0530

    removed unnecessary styling

--- src/assets/jss/components/characterCardStyles.ts
+++ src/assets/jss/components/characterCardStyles.ts
@@ -8,19 +8,7 @@ const styles = {
     height: "800px",
   },
   cardMedia: { height: "200px" },
-  status: {
-    position: "absolute",
-    top: "10px",
-    left: "15px",
-  },
-  iconButton: {
-    // backgroundColor: "#00000020",
-    backdropFilter: "brightness(0.9)",
-    position: "absolute",
-    color: "#FFF",
-    right: 8,
-    top: 8,
-  },
+
   characterName: {
     display: "flex",
     // alignItems: "center",
@@ -34,36 +22,11 @@ const styles = {
     // justifyContent: "space-between",
     flexWrap: "wrap",
   },
-  cardInfoTitle: {
-    textTransform: "uppercase",
-    fontWeight: "bold",
-    color: "secondary.main",
-  },
   cardContent: {
     padding: "1rem",
     display: "flex",
     flexDirection: "column",
     gap: "0.5rem",
-  },
-  chargesSelect: {
-    height: "36px",
-    fontSize: "0.8rem",
-  },
-  chargeItem: {
-    fontSize: "0.8rem",
-  },
-  chargeContainer: {
-    marginTop: "0.5rem",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    gap: "1.5rem",
-  },
-  actionContainer: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginTop: "0.5rem",
   },
   modalStyle: {
     position: "absolute" as "absolute",
